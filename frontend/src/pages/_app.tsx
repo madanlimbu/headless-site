9c4d4866be7ee0f2f425f21ca2170beb343cab64
madan <madan@madanlimbu.com>
2023-06-28 03:28:56 +0100

    Add gtm on __app

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,26 @@
 import "../app/globals.css";
 import type { AppProps } from "next/app";
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PageWrapper>
-      <>
-        <Component {...pageProps} />
-      </>
-    </PageWrapper>
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-5E0MX1F6WZ" />
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-5E0MX1F6WZ');
+      `}
+      </Script>
+      <PageWrapper>
+        <>
+          <Component {...pageProps} />
+        </>
+      </PageWrapper>
+    </>
   );
 }
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,20 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-5E0MX1F6WZ"
-        ></script>
-        <Script id="google-tag-manager">
-          {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-5E0MX1F6WZ');
-  `}
-        </Script>
-
         <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
       <body className="min-h-screen bg-white">
