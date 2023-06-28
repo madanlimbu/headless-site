75a15ba944ccd5abd96cca8c1004dd175913b309
madan <madan@madanlimbu.com>
2023-06-28 04:13:32 +0100

    new gtm script

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,14 +6,13 @@ import Script from "next/script";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-5E0MX1F6WZ" />
       <Script id="google-tag-manager" strategy="afterInteractive">
         {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-5E0MX1F6WZ');
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5G6BGWT');
       `}
       </Script>
       <PageWrapper>
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -9,6 +9,14 @@ export default function Document() {
       <body className="min-h-screen bg-white">
         <Main />
         <NextScript />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5G6BGWT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </Html>
   );
