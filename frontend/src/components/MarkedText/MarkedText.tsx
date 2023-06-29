60c19f3373fc22b29b27304a462c6cf7e29abc24
madan <madan@madanlimbu.com>
2023-06-29 22:06:24 +0100

    Change markdown parser an add mermaid

--- frontend/src/components/MarkedText/MarkedText.tsx
+++ frontend/src/components/MarkedText/MarkedText.tsx
@@ -1,15 +1,45 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import hljs from "highlight.js";
-import { marked } from "marked";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype/lib";
+import rehypeStringify from "rehype-stringify/lib";
+import rehypeMermaid from "rehype-mermaidjs";
 
 export default function MarkedText({ text }: { text: string }): ReactElement {
+  const [hast, setHast] = useState("");
   useEffect(() => {
     hljs.highlightAll();
-  });
+    // mermaid.initialize({
+    //   startOnLoad: false,
+    // });
+    // mermaid.run({ querySelector: ".language-mermaid" });
+    markItUp(text).then((a) => {
+      setHast(a.toString());
+    });
+  }, [text]);
+  // const md = new MarkdownIt();
+  // const parsed_text = md.render(text);
+  // const parsed_text = unified()
+  //   .use(remarkParse)
+  //   .use(remarkRehype)
+  //   .process(text);
+
+  const markItUp = async (text: string) => {
+    return unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeMermaid)
+      .use(rehypeStringify)
+      .process(text);
+  };
+
   return (
     <div
       className="prose max-w-none"
-      dangerouslySetInnerHTML={{ __html: marked.parse(text) }}
+      dangerouslySetInnerHTML={{
+        __html: hast,
+      }}
     />
   );
 }
